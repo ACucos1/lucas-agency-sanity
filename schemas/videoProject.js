25ac715c9d8bc256487c905b8d40ff9c6353e17c
Alex <alexcucos1@gmail.com>
2023-01-20 19:03:29 -0500

    videoProject.js

---
+++ schemas/videoProject.js
@@ -0,0 +1,12 @@
+export default {
+  name: "videoProject",
+  type: "document",
+  title: "Video Project",
+  fields: [
+    {
+      name: "video",
+      type: "file",
+      title: "Video",
+    },
+  ],
+};
